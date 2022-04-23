f694d7f7385494f2c5741527e5c08c9846be4b03
wangyaqi <1069952504@qq.com>
2022-04-23 16:06:43 +0800

    添加基础页面

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.scss'
+import Home from './pages/Layout/index'
+import Login from './pages/Login/index'
 
 export default function App() {
-    return (
-        <div className="app">
-            APP
-        </div>
-    )
+  return (
+    <Router>
+      <div className="app">
+		  <Switch>
+		  	<Route path='/home' component={Home} />
+		  	<Route path='/login' component={Login} />
+		  </Switch>
+	  </div>
+    </Router>
+  )
 }
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,8 @@
+
+export default function Login() {
+    return (
+        <div>
+            登录页面
+        </div>
+    )
+}
